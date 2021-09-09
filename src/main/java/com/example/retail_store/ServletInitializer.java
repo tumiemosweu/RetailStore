732411a87e1a1e508ff2ffeb3b45e21679466694
Itumeleng Mosweu <imosweuk@gmail.com>
2021-09-09 19:59:43 +0200

    add swagger UI image

---
+++ src/main/java/com/example/retail_store/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.retail_store;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RetailStoreApplication.class);
+    }
+
+}
